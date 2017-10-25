967d765df38c4d36c8f0679372d1ed391c4b6d02
JoshKWilliams <32363238+JoshKWilliams@users.noreply.github.com>
2017-10-25 14:19:04 +0100

    Add files via upload

---
+++ Week 3 Exercise 2.py
@@ -0,0 +1,17 @@
+# Number Of Pupils      Number of Teams      Children left out
+#        137                  27.4                   2
+#        628                 125.6                   3
+#        89                   17.8                   4
+#        23                    4.6                   3
+
+
+
+number_of_pupils = 137
+football_teams = number_of_pupils / 5
+print(number_of_pupils)
+print(football_teams)
+x = football_teams
+x = int(x)
+children_in_teams = x * 5
+children_left_out = number_of_pupils - children_in_teams
+print(children_left_out)
---
+++ Week 3 Exercise 7.py
@@ -0,0 +1,3 @@
+name = input ('Please enter your name: ')
+print()
+print ('Hello ' + name + ', it is good to meet you')
